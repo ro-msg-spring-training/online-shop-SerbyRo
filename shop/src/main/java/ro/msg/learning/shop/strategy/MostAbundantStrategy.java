7047b1ed2177182fd8619188f314e0dca03ffdc9
ioness <serbanandrei.ionescu@gmail.com>
2022-08-01 16:56:46 +0300

    Chapter 04: Strategies and adding an order done, resolved most of the modifications from code review

--- shop/src/main/java/ro/msg/learning/shop/strategy/MostAbundantStrategy.java
+++ shop/src/main/java/ro/msg/learning/shop/strategy/MostAbundantStrategy.java
@@ -8,32 +8,36 @@ import ro.msg.learning.shop.repository.LocationStrategy;
 import ro.msg.learning.shop.service.exceptions.ProductException;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 public class MostAbundantStrategy implements LocationStrategy {
     @Autowired
     private IStockInterfaceRepository stockInterfaceRepository;
+
     @Override
     public List<Stock> findBestLocation(ProductOrder order) {
         List<Stock> stockLocation = new ArrayList<>();
-        order.getOrderDetails().forEach(orderDetail->{
-            try{
-                List<Stock> stocks = stockInterfaceRepository.findAllByQuantityAndProductId(orderDetail.getQuantity(),orderDetail.getProduct().getId());
-                if (stocks.isEmpty())
-                {
+        order.getOrderDetails().forEach(orderDetail -> {
+            try {
+                List<Stock> stocks = stockInterfaceRepository.findAllByQuantityAndProductId(orderDetail.getQuantity(), orderDetail.getProduct().getId());
+                if (stocks.isEmpty()) {
                     try {
-                        throw new ProductException("Nu exista produsul cu detaliile cu id-ul "+ orderDetail.getProductId());
+                        throw new ProductException("Nu exista produsul cu detaliile cu id-ul " + orderDetail.getProductId());
                     } catch (ProductException e) {
                         throw new RuntimeException(e);
                     }
                 }
                 Stock stock = stocks.get(0);
-                stockLocation.add(new Stock(stock.getLocation(),stock.getProduct(),stock.getQuantity()));
-            }catch (Exception ex)
-            {
-                throw new RuntimeException("The product with the "+orderDetail.getProduct().getId()+" doesn't exist!");
+                stockLocation.add(new Stock(stock.getLocation(), stock.getProduct(), stock.getQuantity()));
+            } catch (Exception ex) {
+                throw new RuntimeException("The product with the " + orderDetail.getProduct().getId() + " doesn't exist!");
             }
         });
         return stockLocation;
     }
+
+
 }
